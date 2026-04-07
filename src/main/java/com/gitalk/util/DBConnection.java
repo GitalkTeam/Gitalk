cbf9fef7a1442b541d09e198e9e645e8b15ea81f
정경인 <zjrtw12@gmail.com>
2026-04-07 13:20:00 +0900

    feat: 실행환경 세팅 - 윈도우용 컴파일/실행파일 생성 - db 접속 환경 설정

---
+++ src/main/java/com/gitalk/util/DBConnection.java
@@ -0,0 +1,62 @@
+package com.gitalk.util;
+
+/**
+ * DBConnection Description :
+ * NOTE :
+ *
+ * @author jki
+ * @since 04-07 (화) 오후 1:13
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class DBConnection {
+
+    private static String URL;
+    private static String USER;
+    private static String PASSWORD;
+
+    static {
+        try {
+            Properties prop = new Properties();
+            InputStream input = DBConnection.class
+                    .getClassLoader()
+                    .getResourceAsStream("config.properties");
+
+            if (input == null) {
+                throw new RuntimeException("config.properties 파일을 찾을 수 없습니다.");
+            }
+
+            prop.load(input);
+
+            URL = prop.getProperty("db.url");
+            USER = prop.getProperty("db.user");
+            PASSWORD = prop.getProperty("db.password");
+
+        } catch (Exception e) {
+            throw new RuntimeException("프로퍼티 파일 로딩 실패", e);
+        }
+    }
+
+    public static Connection makeConnection() {
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            System.out.println("1. 데이터베이스 연결중 ...");
+            con = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("1. 데이터베이스 연결 성공");
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("JDBC 드라이버를 찾지 못했습니다...");
+        } catch (SQLException e) {
+            System.out.println("데이터베이스 연결 실패");
+            System.out.println(e.getMessage());
+        }
+        return con;
+    }
+}
